ef87efb861c4bced4bf064fc27588f51b7ab2f75
rentbest Asus <rinat.davleev.97@gmail.com>
2024-08-23 12:36:13 +0300

    added frontend

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, Request, Depends 
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
-from app.hotels.router import get_hotels
+from app.hotels.router import get_hotels, get_hotel_by_id
+import app.hotels.exceptions as exc
 
 
 router = APIRouter(
     prefix="/pages",
-    tags=["Фронтенд"],  
+    tags=["Фронтенд"],
 )
-
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -21,4 +21,18 @@ async def get_hotels_page(
     return templates.TemplateResponse(
         name="hotels.html",
         context={"request": request, "hotels": hotels}
+    )
+
+
+@router.get("/hotels/{hotel_id}")
+async def get_hotel_detail_page(
+    request: Request,
+    hotel_id: int,
+    hotel=Depends(get_hotel_by_id),
+):
+    if not hotel:
+        raise exc.HotelNotFound
+    return templates.TemplateResponse(
+        name="hotel_detail.html",
+        context={"request": request, "hotel": hotel}
     )
